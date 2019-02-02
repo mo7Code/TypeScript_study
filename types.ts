9ba95e992133d14bec801c13e0e78a3822ab0c49
Mark <meichangliang@outlook.com>
2019-02-02 17:15:04 +0800

    add

---
+++ types.ts
@@ -0,0 +1,19 @@
+(() => {
+  //布尔值
+  const isDone: boolean = false;
+  const createdByNewBoolean: boolean = Boolean(1);
+
+  // const promise_fn = () => {
+  //   return new Promise((res, rjc) => {
+  //     setTimeout(() => {
+  //       console.log("hello world");
+  //     }, 1000);
+  //   });
+  // };
+
+  // async function await_fn() {
+  //   console.log("ddd");
+  //   await promise_fn();
+  // }
+  // await_fn();
+})();
