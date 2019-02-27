a283476a58baa67b21717a3429b3017e9d6716f8
Mark <meichangliang@outlook.com>
2019-02-27 18:47:09 +0800

    add

--- types.ts
+++ types.ts
@@ -3,17 +3,17 @@
   const isDone: boolean = false;
   const createdByNewBoolean: boolean = Boolean(1);
 
-  // const promise_fn = () => {
-  //   return new Promise((res, rjc) => {
-  //     setTimeout(() => {
-  //       console.log("hello world");
-  //     }, 1000);
-  //   });
-  // };
+  const promise_fn = () => {
+    return new Promise((res, rjc) => {
+      setTimeout(() => {
+        console.log("hello world");
+      }, 1000);
+    });
+  };
 
-  // async function await_fn() {
-  //   console.log("ddd");
-  //   await promise_fn();
-  // }
-  // await_fn();
+  async function await_fn() {
+    console.log("ddd");
+    await promise_fn();
+  }
+  await_fn();
 })();
