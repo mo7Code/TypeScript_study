049967ef9ba8653cf74d8b82a6d20344f7a3e97b
Mark <meichangliang@outlook.com>
2019-05-01 21:47:14 +0800

    add

--- greeter.ts
+++ greeter.ts
@@ -33,11 +33,9 @@ interface Person {
   firstName: string;
   lastName: string;
 }
-
 function greeter(person: Person) {
   return "Hello," + person.firstName + " " + person.lastName;
 }
-
 let user = new Student("Jane", "M.", "User");
 console.log(user);
 
