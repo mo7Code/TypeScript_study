71f2397254b9b9a02e6249be6c660eea0eeeafc4
Mark <meichangliang@outlook.com>
2019-05-01 22:28:13 +0800

    add

---
+++ projectRoot/src/file1.ts
@@ -0,0 +1,25 @@
+class Point {
+  constructor(public x, public y) { }
+  getDistance(p: Point) {
+    let dx = p.x - this.x;
+    let dy = p.y - this.y;
+    return Math.sqrt(dx ** 2 + dy ** 2);
+  }
+}
+// ...
+console.log(Point);
+
+// Reopen the interface.
+interface Point {
+  distanceFromOrigin(point: Point): number;
+}
+
+
+Point.prototype.distanceFromOrigin = function (point: Point) {
+  return this.getDistance({ x: 0, y: 0 });
+}
+
+const lll = new Point(1, 2);
+console.log(lll);
+const rrr = lll.getDistance(2);
+console.log(rrr);
