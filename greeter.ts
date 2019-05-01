368d7a6151a7e54e244677cd463df53804cab3d6
Mark <meichangliang@outlook.com>
2019-05-01 21:45:18 +0800

    add

--- greeter.ts
+++ greeter.ts
@@ -7,6 +7,28 @@
 
 
 
+// interface Person {
+//   firstName: string;
+//   lastName: string;
+// }
+
+// function greeter(person: Person) {
+//   return "Hello," + person.firstName + " " + person.lastName;
+// }
+
+// let user = {
+//   firstName: "Jane",
+//   lastName: "User",
+// }
+// document.body.innerHTML = greeter(user);
+
+class Student {
+  fullName: string;
+  constructor(public firstName, public middleInitial, public lastName) {
+    this.fullName = firstName + '  ' + middleInitial + "  " + lastName;
+  }
+}
+
 interface Person {
   firstName: string;
   lastName: string;
@@ -16,8 +38,7 @@ function greeter(person: Person) {
   return "Hello," + person.firstName + " " + person.lastName;
 }
 
-let user = {
-  firstName: "Jane",
-  lastName: "User",
-}
+let user = new Student("Jane", "M.", "User");
+console.log(user);
+
 document.body.innerHTML = greeter(user);
