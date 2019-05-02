ae3e71348cf89a596dc042b09ac9c20995ecf61d
Mark <meichangliang@outlook.com>
2019-05-03 00:02:54 +0800

    add

--- projectRoot/built/interface.js
+++ projectRoot/built/interface.js
@@ -2,7 +2,6 @@
 //   color?: string;
 //   width?: number;
 // }
-
 // function createSquare(config: SquareConfig): { color: string; area: number } {
 //   let newSquare = { color: "white", area: 100 };
 //   if (config.color) {
@@ -13,28 +12,18 @@
 //   }
 //   return newSquare;
 // }
-
 // let mySquare = createSquare({ color: "black" });
-
-
 // interface SquareConfig {
 //   color: string;
 //   width?: number;
 //   [propName: string]: any;
 // }
-
 // function createSquare(config: SquareConfig): { color: string; area: number } {
 //   let Obj = { ...config, area: 123 };
 //   return Obj;
 //   // ...
 // }
-
 // // let mySquare = createSquare({ color: "red", width: 100, opacity: 0.5 });
-
 // let squareOptions = { color: "red", width: 100, are: 100, asd: 123 };
 // let mySquare = createSquare({ color: "red", width: 100, are: 100, asd: 123 });
-
-
 // 函数类型
-
-
